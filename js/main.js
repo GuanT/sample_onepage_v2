85d6ec39a4bf92f85d0b58d50e94a8ff385689b6
GuanT <gtay060@aucklanduni.ac.nz>
2015-07-15 11:49:31 +1200

    Update main.js
    enabling scrolling back to homepage

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(window).scroll(function(){
 })
 
 $(function(){
-  $('.page-scroll a').bind('click',function(){
+  $('.page-scroll a, .navbar-brand').bind('click',function(){
     var $anchor = $(this);
     $('html,body').stop().animate({
         scrollTop: $($anchor.attr('href')).offset().top
